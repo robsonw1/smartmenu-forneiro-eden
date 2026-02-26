01098d4409ab505d66d28549a746f8a0788c8ed3
robsonw1 <robsonwilliam1008@gmail.com>
2026-02-26 17:38:35 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+// Carrega ferramentas de diagnóstico (disponível em window.__diagnosticSettings)
+import "./lib/diagnostic-settings.ts";
 
 createRoot(document.getElementById("root")!).render(<App />);
